73717ac3b0808d57b094281474516556c4f5d539
Krystian Rozycki <krystian.rozycki@stxnext.pl>
2017-03-15 19:22:06 +0100

    init setup

---
+++ volontulo_org/settings/dev_local.py
@@ -0,0 +1,17 @@
+"""
+Vagrant Development Settings Module
+"""
+
+from .dev import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'volontulo',
+        'USER': 'volontuloapp_org',
+        'PASSWORD': 'volontulo',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
